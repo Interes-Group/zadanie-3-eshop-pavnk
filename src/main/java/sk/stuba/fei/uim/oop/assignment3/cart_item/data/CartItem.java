fdea3ee81bf6a4664d054d069c0d101baa16c295
pavnk <pavlis.nikolas@protonmail.com>
2023-05-11 23:54:36 +0200

    finished Eshop implementation, all tests passed, changed generation strategy to Identity

--- src/main/java/sk/stuba/fei/uim/oop/assignment3/cart_item/data/CartItem.java
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/cart_item/data/CartItem.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Entity
 public class CartItem {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
